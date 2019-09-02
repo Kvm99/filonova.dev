1253c97188c109df40703a2bccdc56ae657db276
Turkova Maria <kvm1990@mail.ru>
2019-09-02 19:44:59 +0300

    deleted "/"

--- resume/urls.py
+++ resume/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.show_main_page, name='show_main_page'),
-    path('/resume/', views.show_resume, name='show_resume'),
-    path('/certificates/', views.show_certificates, name='show_certificates'),
+    path('resume/', views.show_resume, name='show_resume'),
+    path('certificates/', views.show_certificates, name='show_certificates'),
 ]
