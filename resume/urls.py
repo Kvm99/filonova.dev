28441a3db6847cfc28b14b74d9d47e905d3d0de4
Vladimir Filonov <vladimir@filonov.dev>
2023-01-11 17:36:31 +0400

    Create static version of the website

--- resume/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.show_main_page, name='show_main_page'),
-    path('resume/', views.show_resume, name='show_resume'),
-    path('certificates/', views.show_certificates, name='show_certificates'),
-]
